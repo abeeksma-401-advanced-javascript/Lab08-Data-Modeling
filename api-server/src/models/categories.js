7f34eace3a6a4c60ad1859f7087a3ab3fd062dbf
Alexander <maniacallion@gmail.com>
2019-05-30 23:24:52 -0500

    removed old node_modules, reinstalled (now that it lets me), server is able to start again without a million node erros

--- api-server/src/models/categories.js
+++ api-server/src/models/categories.js
@@ -1,19 +1,34 @@
 'use strict';
 
+const Category = require('./categories-schema');
+
 class Categories {
 
-  constructor() {
+//get a category
+  get(_id) {
+//checks for a 24 character id containing 0-9 or a-z as characters (case insensitive)
+    if(!/^[0-9a-z]{24}$/i.test(_id))
+      return Promise.resolve(null)
+
+    return Category.findOne(_id);
   }
 
-  get(_id) {
+  //get all categories
+  getAll(){
+    return Category.find();
   }
   
+//create a category
   post(record) {
+    var category = new Category (record);
+    return category.save();
   }
 
+//update a category?  
   put(_id, record) {
   }
 
+//DESTROY!!!!  
   delete(_id) {
   }
 
